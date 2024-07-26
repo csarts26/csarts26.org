6f4ef7b3d5f1109a674a298fdff8bc16560fca16
tav <booleanman0@gmail.com>
2024-07-25 17:53:10 -0700

    Add Team page

---
+++ src/components/backlink.js
@@ -0,0 +1,9 @@
+export default function BackLink() {
+    return (
+        <div className='flex w-full justify-center mt-20'>
+            <a href='/' className='underline'>
+            Go back to the Main Page
+            </a>
+        </div>
+    )
+}
