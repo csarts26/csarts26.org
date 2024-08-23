1f2275101917b0124aa2cda484ced55f73c580f0
tav <booleanman0@gmail.com>
2024-08-23 09:43:44 -0700

    Remove blurbs from /team

--- src/app/team/page.js
+++ src/app/team/page.js
@@ -1,14 +1,10 @@
 import BackLink from "@/components/backlink";
 
-function TeamCard({ person, image, description }) {
+function TeamCard({ person, image, title }) {
     return (
-      <div className='flex flex-col md:flex-row md:w-[756px] h-fit gap-8 mx-2 md:mx-0 mb-4 text-black bg-white items-start md:items-center py-8 px-8 rounded-lg shadow-md'>
-        <img src='/logo-dark.svg' alt={person.name} className='size-32 bg-yellow aspect-square rounded-full'></img>
-        <div className='flex flex-col'>
-          <h1 className='text-2xl font-medium'>{person.name}</h1>
-          <h2 className='italic opacity-70 mb-2'>{person.title}</h2>
-          <p className='text-sm md:text-base'>{description}</p>
-        </div>
+      <div className="flex flex-col">
+        <p className='text-sm md:text-base font-bold'>{person}</p>
+        <p className='text-sm md:text-base opacity-70 italic'>{title}</p>
       </div>
     )
   }
@@ -20,7 +16,6 @@ return (
         <h1 className='font-medium text-3xl text-brown text-pretty'>Meet the Class of '26 Team</h1>
     </div>
     
-    {/* Person 1 & 2*/}
     <div className='flex flex-col gap-8 md:w-[756px] h-fit mx-2 md:mx-0 mb-4 text-black bg-white items-center py-8 px-8 rounded-lg shadow-md'>
         <img className='w-full h-64 bg-yellow rounded-md'/>
         <div className='flex flex-col'>
@@ -30,26 +25,30 @@ return (
         </div>
     </div>
 
-    <TeamCard 
-        person={{ name: 'Macy Mananquil', title: 'Event Coordinator' }}  
-        image=''
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." 
-    />
-    <TeamCard 
-        person={{ name: 'Mordie Gomez', title: 'Social Manager' }}  
-        image=''
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." 
-    />
-    <TeamCard 
-        person={{ name: 'Octavia Roberts', title: 'Art Director, Developer' }}  
-        image=''  
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." 
-    />
-    <TeamCard 
-        person={{ name: 'Natalie Lanza', title: 'Social Manager' }}  
-        image=''   
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." 
-    />
+    <div className='flex flex-col md:flex-row md:w-[756px] h-fit gap-8 mx-2 md:mx-0 mb-4 text-black bg-white items-start md:items-center py-8 px-8 rounded-lg shadow-md'>
+      <div className='flex flex-col gap-4'>
+        <h1 className='text-xl font-medium'>Class of '26 Cabinet</h1>
+
+        <TeamCard 
+          person="Macy Mananquil"
+          title="Event Coordinator"
+        />
+        <TeamCard 
+          person="Octavia Roberts"
+          title="Art Director, Developer"
+        />
+        <TeamCard 
+          person="Mordie Gomez"
+          title="Social Manager"
+        />
+        <TeamCard 
+          person="Natalie Lanza"
+          title="Social Manager"
+        />
+
+      </div>
+    </div>
+
     <BackLink />
     </main>
 );
